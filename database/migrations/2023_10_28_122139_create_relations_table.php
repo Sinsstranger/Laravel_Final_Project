4c9a7e0f4769dbcb0528fc9a1cf748b7c04f630a
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-28 18:27:58 +0500

    Сделал миграцию для таблицы relations (арендные отношения)

---
+++ database/migrations/2023_10_28_122139_create_relations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('relations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')
+                ->constrained('users');
+            $table->foreignId('tenant_id')
+                ->constrained('users');
+            $table->timestamp('started_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('relations');
+    }
+};
