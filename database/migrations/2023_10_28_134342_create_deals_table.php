28af4996a22153ac43cab5e4cf6f95a791d64796
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-28 19:00:18 +0500

    Создал миграции для таблиц договоров, сделок и отзывов

---
+++ database/migrations/2023_10_28_134342_create_deals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deals', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('rent_starts_at');
+            $table->timestamp('rent_ends_at');
+            $table->decimal('rent_costs', 11, 2);
+            $table->foreignId('status_id')
+                ->constrained('deal_statuses');
+            $table->foreignId('property_id')
+                ->constrained('properties');
+            $table->foreignId('rent_id')
+                ->constrained('relations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deals');
+    }
+};
---
+++ database/migrations/2023_10_28_135317_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->smallInteger('rating');
+            $table->foreignId('author_id')
+                ->constrained('users');
+            $table->foreignId('property_id')
+                ->constrained('properties');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
