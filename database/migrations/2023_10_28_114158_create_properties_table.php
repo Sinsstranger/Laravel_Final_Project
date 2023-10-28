685cf3ccee57af68693f6d2661916affc6cfb038
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-28 16:51:12 +0500

    Добавил миграцию для таблицы properties

---
+++ database/migrations/2023_10_28_114158_create_properties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->text('description');
+            $table->decimal('price_per_day', 9, 2);
+            $table->boolean('is_temporary_registration_possible');
+            $table->foreignId('address_id')
+                ->constrained('addresses')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
