de022f3679b7b8b0d53b89a4dc9c07469c2a328e
ZoyaKapustiak <102512424+ZoyaKapustiak@users.noreply.github.com>
2023-10-30 23:13:39 +0300

    Добавлены компонент head, модели Deal, DealStatus

---
+++ app/Models/Deal.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Deal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'rent_starts_at',
+        'rent_ends_at',
+        'rent_costs',
+        'status_id',
+        'property_id',
+        'rent_id'
+    ];
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(DealStatus::class, 'status_id', 'id');
+    }
+    public function property(): HasMany
+    {
+        return $this->hasMany(Property::class, 'property_id', 'id');
+    }
+    public function rent(): HasMany
+    {
+        return $this->hasMany(Relation::class, 'rent_id', 'id');
+    }
+}
