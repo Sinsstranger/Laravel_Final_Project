10df6e18ff20818f3a015cdd8f579834e831ccdf
sinstranger <sinstranger@yandex.ru>
2023-10-31 21:00:19 +0300

    Delete chirps branch

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
-    ->middleware(['auth', 'verified']);
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
