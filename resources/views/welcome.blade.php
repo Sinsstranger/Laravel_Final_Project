7883d1dc5072651f626f5b62f7dc401939a77161
ZoyaKapustiak <102512424+ZoyaKapustiak@users.noreply.github.com>
2023-10-31 09:09:13 +0300

    Мелкие правки по компоненту head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@if (isset($title))
 <x-head name="head" :title="$title"></x-head>
+@endif
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
