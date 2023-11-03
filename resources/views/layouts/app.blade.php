efe064dcc7ada1930acb2c8c9d60e6b9cc07908d
sinstranger <sinstranger@yandex.ru>
2023-11-03 12:49:40 +0300

    Восстановил Dashboard, Добавил ссылку на админку для авторизованных админов

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <!-- Scripts -->
-        {{--@vite([
+        @vite([
                 'resources/css/open-iconic-bootstrap.min.css',
                 'resources/css/animate.css',
                 'resources/css/owl.carousel.min.css',
@@ -40,7 +40,7 @@
                 'resources/js/scrollax.min.js',
                 'resources/js/main.js',
                 'resources/js/app.js'
-                ])--}}
+                ])
 
     </head>
     <body class="font-sans antialiased">
