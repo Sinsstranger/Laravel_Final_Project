2445db0f3a9803273face8eddea5781d75064b34
Sereban4o <106669883+Sereban4o@users.noreply.github.com>
2023-11-08 20:37:01 +0300

    Перенес в компоненты
    Перенес фильтр в компонент filter
    Перенес каталог в компонент catalog
    Перенес карточку каталога в компонент catalog_card

---
+++ resources/views/components/catalog_card.blade.php
@@ -0,0 +1,19 @@
+<div class="item">
+    <div class="properties ftco-animate">
+        <div class="img">
+            <img src="assets/images/work-<?php echo(rand(1,9)); ?>.jpg" class="img-fluid" alt="Дом для аренды">
+        </div>
+        <div class="desc">
+            <div class="d-flex pt-5">
+                <div>
+                    <h5 class="title"><a href="{{ route('properties.show', $property) }}">{{ $property->title }}</a>
+                    </h5>
+                </div>
+                <div class="pl-md-4">
+                    <h5 class="price">{{ $property->price_per_day}}₽</h5>
+                </div>
+            </div>
+            <p class="h-info"><span class="location">Москва</span></p>
+        </div>
+    </div>
+</div>
