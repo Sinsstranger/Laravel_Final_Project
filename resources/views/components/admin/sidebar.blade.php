9274cd8921277700bd3e30f9d18a7e609df41cad
Kosmos258 <maksimowkirill@gmail.com>
2023-11-17 19:15:16 +0300

    Правки дашборда

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -19,10 +19,6 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                     Графики
                 </a>
-                <a class="nav-link" href="tables.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                    Таблицы
-                </a>
             </div>
         </div>
     </nav>
