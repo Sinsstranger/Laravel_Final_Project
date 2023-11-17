40e089bd254a52b383b8aca7b6d92e307a8167fb
Kosmos258 <maksimowkirill@gmail.com>
2023-11-17 19:15:16 +0300

    Правки главной страницы профиля
    Правки admin

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -14,15 +14,6 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Объявления
                 </a>
-                <div class="sb-sidenav-menu-heading">Дополнения</div>
-                <a class="nav-link" href="charts.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                    Графики
-                </a>
-                <a class="nav-link" href="tables.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                    Таблицы
-                </a>
             </div>
         </div>
     </nav>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,47 +3,7 @@
     @parent
     <link rel="stylesheet" href="{{ asset("assets/css/cabinet.css") }}">
     @endsection
-    {{--<x-slot name="header">
-         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Мои объявления') }}
-    </h2>
-    </x-slot>--}}
-
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex">
-                <div class="avatar-block">
-                    <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <path fill="#000000" d="M628.736 528.896A416 416 0 0 1 928 928H96a415.872 415.872 0 0 1 299.264-399.104L512 704l116.736-175.104zM720 304a208 208 0 1 1-416 0 208 208 0 0 1 416 0z"></path>
-                        </g>
-                    </svg>
-                </div>
-                <ul class="max-w-xl">
-                    <li class="dashboard-link">
-                        <h2 class="text-lg font-medium text-gray-900">
-                            {{$user->name}}
-                        </h2>
-                    </li>
-                    <li class="dashboard-link">
-                        <h2 class="text-lg font-medium text-gray-900">
-                            {{$user->email}}
-                        </h2>
-
-                    </li>
-                    <li class="dashboard-link">
-                        <h2 class="text-lg font-medium text-gray-900">
-                            {{$user->phone}}
-                        </h2>
-
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div> --}}
-
+  
     <div class="container-xl px-4 mt-4">
         <hr class="mt-0 mb-4">
         <div artisan cache:clearv class="row">
@@ -83,7 +43,7 @@
                             <div class="row gx-3 mb-3">
                                 <div class="col-md-6">
                                     <label class="small mb-1" for="inputPhone">Номер телефона</label>
-                                    <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="{{$user->phone}}">
+                                    <input class="form-control" id="inputPhone" type="tel" placeholder="Введите ваш номер телефона" value="{{$user->phone}}">
                                 </div>
                             </div>
                             <button class="btn btn-primary" type="button">Сохранить изменения</button>
