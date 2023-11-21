441ea1ce01c13c64d02659c3e3f9820709adcbe4
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-11-21 23:39:12 +0500

    Добавил crud-блок работы со статусами заявок - теперь в админке статусы можно редактировать, добавлять, и удалять. - реализовал soft-deleting - в сидер статусов добавил пятый статус - "назначен показ"

--- database/migrations/2023_10_28_133732_create_deal_statuses_table.php
+++ database/migrations/2023_10_28_133732_create_deal_statuses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 30)->default(1)->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
---
+++ resources/views/admin/dealStatuses/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts/admin')
+@section('content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Редактор категории</h1>
+    @include('inc.message')
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-table me-1"></i>
+            {{-- {{ $dealStatus->name }} --}}
+        </div>
+
+        <form method="post"
+        action="@if(empty($dealStatus)){{ route('admin.dealStatuses.store') }}
+                @else{{ route('admin.dealStatuses.update', $dealStatus) }}
+                @endif"
+        enctype="multipart/form-data">
+
+        @csrf
+
+            @if (isset($dealStatus)) @method('PUT')
+            @else @method('POST')
+            @endif
+
+            <div class="form-group">
+                <label for="name">Название</label>
+                <input type="text" name="name" class="form-control" id="name" value="{{ $dealStatus->name ?? old('name') }}">
+                {{-- @error('name') <div id="validationServerUsernameFeedback" class="invalid-feedback">{{ $message }}</div>@enderror --}}
+            </div>
+            <button type="submit" class="btn btn-success">Добавить</button>
+        </form>
+    </div>
+</div>
+
+@endsection
