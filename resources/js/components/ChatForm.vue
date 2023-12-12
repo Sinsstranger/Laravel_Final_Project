26366ea2865078c4d25b6a8484eb2a65934021a2
ZoyaKapustiak <102512424+ZoyaKapustiak@users.noreply.github.com>
2023-12-13 00:30:12 +0300

    Валидация сообщений, отправка сообщений по клику на button, стили на форму отправки

--- resources/js/components/ChatForm.vue
+++ resources/js/components/ChatForm.vue
@@ -10,7 +10,7 @@
                placeholder="Напишите сообщение"
                @keyup.enter="sendMessage"
                class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-full py-3">
-            <button @click="sendMessage" type="button">
+            <button @click="sendMessage" type="button" class="key_up_button">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="black" class="h-6 w-6 transform rotate-90">
                     <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
                 </svg>
@@ -40,14 +40,7 @@ export default {
         });
 
         const sendMessage = async (e) => {
-            if (e.target.nodeName == "INPUT") {
-                await addMessage({
-                    message: e.target.value,
-                    chat: props.chat.id
-                })
-            }
-            console.log(e);
-            // await addMessage(form);
+            await addMessage(form);
             form.message = '';
         }
         return {
